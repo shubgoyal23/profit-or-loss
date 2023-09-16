c9ccde99e567d67a7c87c2deef51502a41066296
shubham <124563388+shubgoyal23@users.noreply.github.com>
2023-09-16 07:45:28 +0530

    css fix

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ const output = document.querySelector("#output-message")
 submitbtn.addEventListener("click", function plcalculator(){
     let priceChange = Number(currentPrice.value) - Number(initialPrice.value);
     let pfl = priceChange * 100 / Number(initialPrice.value);
-    if(pfl > 0){
-        output.innerHTML = `the profit is ${pfl.toFixed(2)}% and total profits is ${(pfl * Number(quantity.value)).toFixed(2)}`;
+    if(priceChange > 0){
+        output.innerHTML = `Congrats You made a profit of ${pfl.toFixed(2)}% on your trade, and your total profit is ${(priceChange * Number(quantity.value)).toFixed(2)}`;
         output.style.color = "rgb(44, 177, 44)"
-    }else if(pfl < 0){
-        output.innerHTML = `the loss % is ${-pfl.toFixed(2)} and total Losses is ${(-priceChange * Number(quantity.value)).toFixed(2)}`;
+    }else if(priceChange < 0){
+        output.innerHTML = `Sorry for the ${-pfl.toFixed(2)}% loss, and your total Loss is ${(-priceChange * Number(quantity.value)).toFixed(2)}`;
         output.style.color = "rgb(216, 52, 52)"
     }else{
         output.innerHTML = `No profit no Loss`;
